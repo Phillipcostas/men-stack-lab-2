2df6dec10b6b1785a124a6861115f19c7715a9ce
PhillipCostas <Phillipcostas@gmail.com>
2024-06-05 20:31:38 -0400

    changed items back to what the lecture was, still having problems with it

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const dotenv = require("dotenv"); // require package
     dotenv.config(); // Loads the environment variables from .env file
 const express = require("express");
@@ -16,83 +15,147 @@ console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 
 
 const Food = require("./models/food.js");
+
+
+// <---------------------- middleware - routes -------------------------->
+
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method")); // new
 app.use(morgan("dev")); //new
 
-// <----------------------- Routes -------------->
+//<----------------------- Get - routes --------------------------------->
+
+
 app.get("/", async (req, res) => {
     res.render('index.ejs');
   });
 
-// GET /food/new
-app.get("foods/new", (req, res) => {
-  res.render('/foods/new.ejs');
-});
-
-  app.get ('foods', (req, res) => {
-    res.render('/foods/allfood.ejs');
-  })
-
-
-// GET /foods
-app.get("/foods", async (req, res) => {
-    const allFoods = await Food.find();
-    console.log(allFoods);
-    res.render("foods/allfood.ejs", { food: allFoods });
+  app.get("/foods", async (req, res) => {
+    const allFood = await Food.find();
+    // console.log(allFoods)
+    res.render('foods/index.ejs', { food: allFood });
   });
 
-  app.get("/foods/:foodId", async (req, res) => {
-    const foundFood = await Food.findById(req.params.fruitId);
-    res.render("foods/edit.ejs", { food: foundFood });
-  });
+  app.get('/foods/new', (req, res) => {
+    res.render('foods/new.ejs');
+})
 
-  // GET localhost:3000/fruits/:fruitId/edit
-  app.get("/foods/:foodId/edit", async (req, res) => {
-    const foundFood = await Food.findById(req.params.foodId);
-    console.log(foundFood);
-    res.render("foods/edit.ejs", {
-        fruit: foundFood,
-      });
-    });
-
-
-  // POST /foods
+// POST /foods
 app.post("/foods", async (req, res) => {
   if (req.body.isYourFavortie === "on") {
-      req.body.isYourFavortie = true;
-    } else {
-      req.body.isYourFavortie = false;
-    }
-    await Food.create(req.body);
-    res.redirect("/foods"); //redirect to index foods
-  });
+    req.body.isYourFavortie = true;
+  } else {
+    req.body.isYourFavortie = false;
+  }
+  await Food.create(req.body);
+  res.redirect("/foods");
+});
 
-  app.put("/foods/:foodId", async (req, res) => {
-    // Handle the 'isReadyToEat' checkbox data
-    if (req.body.isYourFavortie === "on") {
-      req.body.isYourFavortie = true;
-    } else {
-      req.body.isYourFavortie = false;
-    }
-    
-    // Update the food in the database
-    await Food.findByIdAndUpdate(req.params.foodId, req.body);
-  
-    // Redirect to the food show page to see the updates
-    res.redirect(`/foods/${req.params.foodId}`);
-  });
+app.get("/foods/:foodId", async (req, res) => {
+  const foundFood = await Food.findById(req.params.foodId);
+  res.render('foods/show.ejs', { food: foundFood });
+});
 
 
+app.get('/foods/:foodId/edit', async(req, res) => {
+  const foundFood = await Food.findById(req.params.foodId)
+  console.log(foundFood);
+  res.render("foods/edit.ejs", {
+      food: foundFood,    
+})
+})
+//<--------------------- Edit / Delete - Route ------------------------->
 
-  app.delete("/foods/:foodId", async (req, res) => {
-    await Food.findByIdAndDelete(req.params.foodId);
-    res.redirect("/foods");
-  });
+app.put("/food/:foodId", async (req, res) => {
+  if (req.body.isYourFavortie === "on") {
+    req.body.isYourFavortie = true;
+  } else {
+    req.body.isYourFavortie = false;
+  }
+  await Food.findByIdAndUpdate(req.params.foodId, req.body);
 
+  res.redirect(`/foods/${req.params.foodId}`);
+});
 
+app.delete("/foods/:foodId", async (req, res) => {
+  await Food.findByIdAndDelete(req.params.fruitId);
+  res.redirect('/foods');
+});
 
-
+//<--------------------- Listening - Route --------------------------->
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
+
+// // // GET /food/new
+// // app.get("foods/new", (req, res) => {
+// //   res.render('/foods/new.ejs');
+// // });
+
+//   // app.get ('foods', (req, res) => {
+//   //   res.render('/foods/allfood.ejs');
+//   // })
+
+
+// // GET /foods
+// app.get("/foods", async (req, res) => {
+//     const allFoods = await Food.find();
+//     console.log(allFoods);
+//     res.render("foods/allfood.ejs", { food: allFoods });
+//   });
+
+//   app.get("/foods/:foodId", async (req, res) => {
+//     const foundFood = await Food.findById(req.params.fruitId);
+//     res.render("foods/edit.ejs", { food: foundFood });
+//   });
+
+//   // GET localhost:3000/fruits/:fruitId/edit
+//   app.get("/foods/:foodId/edit", async (req, res) => {
+//     const foundFood = await Food.findById(req.params.foodId);
+//     console.log(foundFood);
+//     res.render("foods/edit.ejs", {
+//         fruit: foundFood,
+//       });
+//     });
+
+
+//   // POST /foods
+// app.post("/foods", async (req, res) => {
+//   if (req.body.isYourFavortie === "on") {
+//       req.body.isYourFavortie = true;
+//     } else {
+//       req.body.isYourFavortie = false;
+//     }
+//     await Food.create(req.body);
+//     res.redirect("/foods"); //redirect to index foods
+//   });
+
+//   app.put("/foods/:foodId", async (req, res) => {
+//     // Handle the 'isReadyToEat' checkbox data
+//     if (req.body.isYourFavortie === "on") {
+//       req.body.isYourFavortie = true;
+//     } else {
+//       req.body.isYourFavortie = false;
+//     }
+    
+//     // Update the food in the database
+//     await Food.findByIdAndUpdate(req.params.foodId, req.body);
+  
+//     // Redirect to the food show page to see the updates
+//     res.redirect(`/foods/${req.params.foodId}`);
+//   });
+
+
+
+//   app.delete("/foods/:foodId", async (req, res) => {
+//     await Food.findByIdAndDelete(req.params.foodId);
+//     res.redirect("/foods");
+//   });
+
+
+
+
+// app.listen(3000, () => {
+//   console.log("Listening on port 3000");
+// });
